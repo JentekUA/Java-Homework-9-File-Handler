12160df039a33b68fd0170814c2fa85e3ae60e2c
oleksandr.aleksashyn <oleksandr.aleksashyn@gmail.com>
2024-11-25 15:43:13 +0200

    Add functionality to work with files
    * Constants with BASE_PATH
    * Class that handles read/write operations
    * Read write functionality in Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,21 @@
 package app;
 
+import static app.Constants.BASE_PATH;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println();
+        FileHandler handler = new FileHandler();
+        String fileName = "myfile";
+        String fileContent = "My very important information.";
+        String result = handler.writeFile(fileName, fileContent);
+        String content = handler.readFile(BASE_PATH + fileName + ".txt");
+        getOutput("RESULT: " + result);
+        getOutput("FILE CONTENT: " + content);
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
     }
 }
